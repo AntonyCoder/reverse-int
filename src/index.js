9b4f52839d68b1bcca436a48a3a4b478d4fbf293
AntonyCoder <lysenko.tosha@gmail.com>
2021-02-20 16:16:06 +0300

    feat: add solution reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
-}
+   if (n < 0) {
+        n *= -1;
+    }
+
+    let str = String(n);
+    let newStr = '';
+
+    for(let i = 0; i < str.length; i++) {
+        newStr = str[i] + newStr;
+    }
+
+    let newNum = Number(newStr);
+    return newNum;  
+};
